bc08fa44cbd888d1a07c8840bd257c5757b6b345
HanzalahnurSolikhin <hanzalahnursolikhin@gmail.com>
2025-05-26 06:51:18 +0800

    Update Stack_FinalSolution.c

--- D121241110_Hanzalahnur Solikhin/Stack_FinalSolution.c
+++ D121241110_Hanzalahnur Solikhin/Stack_FinalSolution.c
@@ -1,84 +1,44 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <stack>
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
+int main() {
+    int Q;
+    cin >> Q;
 
-vector<int> getMax(const vector<string>& operations) {
-    stack<int> s, maxS;
-    vector<int> result;
+    string current = "";              // Menyimpan teks saat ini
+    stack<string> history;           // Menyimpan riwayat untuk undo
 
-    for (const auto& op : operations) {
-        if (op[0] == '1') {
-            int x = stoi(op.substr(2));
-            s.push(x);
-            maxS.push(maxS.empty() ? x : max(x, maxS.top()));
-        } else if (op[0] == '2') {
-            s.pop();
-            maxS.pop();
-        } else if (op[0] == '3') {
-            result.push_back(maxS.top());
+    while (Q--) {
+        int op;
+        cin >> op;
+
+        if (op == 1) {
+            string add;
+            cin >> add;
+            history.push(current);   // Simpan keadaan sebelum perubahan
+            current += add;          // Tambahkan teks
+        } 
+        else if (op == 2) {
+            int k;
+            cin >> k;
+            history.push(current);   // Simpan keadaan sebelum perubahan
+            current.erase(current.size() - k); // Hapus k karakter dari belakang
+        } 
+        else if (op == 3) {
+            int k;
+            cin >> k;
+            if (k >= 1 && k <= current.size()) {
+                cout << current[k - 1] << '\n'; // Cetak karakter ke-k
+            }
+        } 
+        else if (op == 4) {
+            if (!history.empty()) {
+                current = history.top(); // Undo: kembalikan string sebelumnya
+                history.pop();
+            }
         }
     }
-
-    return result;
-}
-
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    vector<string> ops(n);
-
-    for (int i = 0; i < n; i++) {
-        string ops_item;
-        getline(cin, ops_item);
-
-        ops[i] = ops_item;
-    }
-
-    vector<int> res = getMax(ops);
-
-    for (size_t i = 0; i < res.size(); i++) {
-        fout << res[i];
-
-        if (i != res.size() - 1) {
-            fout << "\n";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
 
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
 }
