466c83b1a730a31d1ca292f74046fa4160d7b465
HanzalahnurSolikhin <hanzalahnursolikhin@gmail.com>
2025-05-26 05:17:31 +0800

    Update Stack_AiSolution.c

--- D121241110_Hanzalahnur Solikhin/Stack_AiSolution.c
+++ D121241110_Hanzalahnur Solikhin/Stack_AiSolution.c
@@ -1,186 +1,93 @@
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <bits/stdc++.h>
 
-char* readline();
-char* ltrim(char*);
-char* rtrim(char*);
+using namespace std;
 
-int parse_int(char*);
+string ltrim(const string &);
+string rtrim(const string &);
 
-int* getMax(int operations_count, char** operations, int* result_count) {
-    int* result = malloc(operations_count * sizeof(int));
-    int resultIndex = 0;
+vector<int> getMax(vector<string> operations) {
+stack<int> mainStack;    
+    stack<int> maxStack;    
+    vector<int> result;      
+    
+    for (const string& op : operations) {
+        if (op[0] == '1') { 
+            int x = stoi(op.substr(2));
+            mainStack.push(x); 
 
-    int* stack = malloc(operations_count * sizeof(int));
-    int* maxStack = malloc(operations_count * sizeof(int));
-    int top = -1;
-    int maxTop = -1;
-
-    for (int i = 0; i < operations_count; i++) {
-        if (operations[i][0] == '1') {
-            int x;
-            sscanf(operations[i], "1 %d", &x);
-
-            stack[++top] = x;
-
-            if (maxTop == -1 || x >= maxStack[maxTop]) {
-                maxStack[++maxTop] = x;
+            if (maxStack.empty() || x >= maxStack.top()) {
+                maxStack.push(x);
             } else {
-                maxStack[++maxTop] = maxStack[maxTop];
+                maxStack.push(maxStack.top());
             }
-
-        } else if (operations[i][0] == '2') {
-            if (top >= 0) {
-                top--;
-                maxTop--;
+        } else if (op[0] == '2') { 
+            if (!mainStack.empty()) {
+                mainStack.pop();  
+                maxStack.pop(); 
             }
-        } else if (operations[i][0] == '3') {
-            if (maxTop >= 0) {
-                result[resultIndex++] = maxStack[maxTop];
+        } else if (op[0] == '3') { 
+            if (!maxStack.empty()) {
+                result.push_back(maxStack.top()); 
             }
         }
     }
 
-    *result_count = resultIndex;
-    return result;
-
+    return result; 
 }
 
 int main()
 {
-    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-    int n = parse_int(ltrim(rtrim(readline())));
+    string n_temp;
+    getline(cin, n_temp);
 
-    char** ops = malloc(n * sizeof(char*));
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> ops(n);
 
     for (int i = 0; i < n; i++) {
-        char* ops_item = readline();
+        string ops_item;
+        getline(cin, ops_item);
 
-        *(ops + i) = ops_item;
+        ops[i] = ops_item;
     }
 
-    int res_count;
-    int* res = getMax(n, ops, &res_count);
+    vector<int> res = getMax(ops);
 
-    for (int i = 0; i < res_count; i++) {
-        fprintf(fptr, "%d", *(res + i));
+    for (size_t i = 0; i < res.size(); i++) {
+        fout << res[i];
 
-        if (i != res_count - 1) {
-            fprintf(fptr, "\n");
+        if (i != res.size() - 1) {
+            fout << "\n";
         }
     }
 
-    fprintf(fptr, "\n");
+    fout << "\n";
 
-    fclose(fptr);
+    fout.close();
 
     return 0;
 }
 
-char* readline() {
-    size_t alloc_length = 1024;
-    size_t data_length = 0;
+string ltrim(const string &str) {
+    string s(str);
 
-    char* data = malloc(alloc_length);
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
 
-    while (true) {
-        char* cursor = data + data_length;
-        char* line = fgets(cursor, alloc_length - data_length, stdin);
-
-        if (!line) {
-            break;
-        }
-
-        data_length += strlen(cursor);
-
-        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
-            break;
-        }
-
-        alloc_length <<= 1;
-
-        data = realloc(data, alloc_length);
-
-        if (!data) {
-            data = '\0';
-
-            break;
-        }
-    }
-
-    if (data[data_length - 1] == '\n') {
-        data[data_length - 1] = '\0';
-
-        data = realloc(data, data_length);
-
-        if (!data) {
-            data = '\0';
-        }
-    } else {
-        data = realloc(data, data_length + 1);
-
-        if (!data) {
-            data = '\0';
-        } else {
-            data[data_length] = '\0';
-        }
-    }
-
-    return data;
+    return s;
 }
 
-char* ltrim(char* str) {
-    if (!str) {
-        return '\0';
-    }
+string rtrim(const string &str) {
+    string s(str);
 
-    if (!*str) {
-        return str;
-    }
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
 
-    while (*str != '\0' && isspace(*str)) {
-        str++;
-    }
-
-    return str;
-}
-
-char* rtrim(char* str) {
-    if (!str) {
-        return '\0';
-    }
-
-    if (!*str) {
-        return str;
-    }
-
-    char* end = str + strlen(str) - 1;
-
-    while (end >= str && isspace(*end)) {
-        end--;
-    }
-
-    *(end + 1) = '\0';
-
-    return str;
-}
-
-int parse_int(char* str) {
-    char* endptr;
-    int value = strtol(str, &endptr, 10);
-
-    if (endptr == str || *endptr != '\0') {
-        exit(EXIT_FAILURE);
-    }
-
-    return value;
+    return s;
 }
